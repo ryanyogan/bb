e5f0bf59660a30240f4e667cf63791e0e6b5d641
Ryan Yogan <ryanyogan@gmail.com>
2019-06-03 13:24:48 -0700

    Basics wired up

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index Page</title>
+      <meta name="description" content="This is the index page" />
+    </Head>
+    <p>Index Page Brah</p>
+  </div>
+);
+
+export default Index;
