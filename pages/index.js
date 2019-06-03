78bac1a2bada47225131402d737e0ebb269ed37b
Ryan Yogan <ryanyogan@gmail.com>
2019-06-03 14:49:08 -0700

    added simple server, wiring up some more goodies

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
-import { Button } from '@material-ui/core';
 import Head from 'next/head';
+import { shape, string } from 'prop-types';
 import withLayout from '../lib/withLayout';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index Page</title>
       <meta name="description" content="This is the index page" />
     </Head>
     <p>Index Page Brah</p>
-    <Button variant="contained">MUI</Button>
+    <p>
+      Email:
+      {user.email}
+    </p>
   </div>
 );
+
+Index.getInitialProps = async ({ query }) => ({ user: query.user });
+
+Index.propTypes = {
+  user: shape({
+    email: string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default withLayout(Index);
