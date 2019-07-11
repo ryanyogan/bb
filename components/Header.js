e38547729efe54e87ef2cb19a4e60ab5c32b54a9
Ryan Yogan <ryanyogan@gmail.com>
2019-07-10 20:41:19 -0700

    Updated session store, added menu drop, header receives props

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,71 @@
-import { Grid, Toolbar } from '@material-ui/core';
+import { Avatar, Grid, Hidden, Toolbar } from '@material-ui/core';
 import Link from 'next/link';
+import { shape, string } from 'prop-types';
 import React from 'react';
+import MenuDrop from './MenuDrop';
 import { styleToolbar } from './SharedStyles';
 
-const Header = () => (
+const optionsMenu = [
+  {
+    text: 'Got Questions?',
+    href: 'https://github.com/builderbook/builderbook/issues',
+  },
+  {
+    text: 'Log out',
+    href: '/logout',
+  },
+];
+
+const Header = ({ user }) => (
   <div>
     <Toolbar style={styleToolbar}>
       <Grid container direction="row" justify="space-around" align="center">
-        <Grid item xs={12} style={{ textAlign: 'right' }}>
-          <Link prefetch href="/login">
-            <a style={{ margin: '0px 20px 0px auto' }}>Log in</a>
-          </Link>
+        <Grid item sm={10} xs={9} style={{ textAlign: 'left' }}>
+          {user ? (
+            <div>
+              <Hidden smDown>
+                <Link prefetch href="/">
+                  <a style={{ marginRight: '20px' }}>Settings</a>
+                </Link>
+              </Hidden>
+            </div>
+          ) : (
+            <Link prefetch href="/">
+              <Avatar
+                src="https://storage.googleapis.com/builderbook/logo.svg"
+                alt="Builder Book Logo"
+                style={{ margin: '0px auto 0px 20px' }}
+              />
+            </Link>
+          )}
+        </Grid>
+        <Grid item sm={1} xs={3} style={{ textAlign: 'right' }}>
+          {user ? (
+            <div style={{ whiteSpace: 'nowrap' }}>
+              {user.avatarUrl && (
+                <MenuDrop options={optionsMenu} src={user.avatarUrl} alt={user.displayName} />
+              )}
+            </div>
+          ) : (
+            <Link prefetch href="/login">
+              <a style={{ margin: '0px 20px 0px auto' }}>Log in</a>
+            </Link>
+          )}
         </Grid>
       </Grid>
     </Toolbar>
   </div>
 );
+
+Header.propTypes = {
+  user: shape({
+    avatarUrl: string,
+    displayName: string,
+  }),
+};
+
+Header.defaultProps = {
+  user: null,
+};
 
 export default Header;
--- server/app.js
+++ server/app.js
@@ -1,18 +1,54 @@
+require('dotenv').config();
+
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoSessionStore = require('connect-mongo');
+
+const User = require('./models/User');
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect(MONGO_URL, options);
 
 const PORT = process.env.PORT || 8000;
 const ROOT_URL = `http://localhost:${PORT}`;
 
-const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/', (req, res) => {
-    const user = { email: 'ryanyogan@gmail.com' };
+  const MongoStore = mongoSessionStore(session);
+
+  const sess = {
+    name: 'builderbook.sid',
+    secret: 'foobarbuttskins22312313',
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60, // 14 days
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 14 * 24 * 60 * 60 * 100, // 14 days
+    },
+  };
+
+  server.use(session(sess));
+
+  server.get('/', async (req, res) => {
+    const user = await User.findOne({ slug: 'ryan-yogan' });
     app.render(req, res, '/', { user });
   });
 
