a4af8ce5bed8de337f840c3fcd89e0e641ab3ea6
Ryan Yogan <ryanyogan@gmail.com>
2019-07-10 18:31:12 -0700

    added in a custom server

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
 import Head from 'next/head';
+import { shape, string } from 'prop-types';
 import React from 'react';
 
 class Index extends React.Component {
   state = {};
 
+  static propTypes = {
+    user: shape({
+      email: string.isRequired,
+    }),
+  };
+
+  static defaultProps = {
+    user: null,
+  };
+
+  static async getInitialProps(ctx) {
+    return {
+      user: ctx.query.user,
+    };
+  }
+
+  static getInitialProps = async (ctx) => ({
+    user: ctx.query.user,
+  });
+
   render() {
+    const { user } = this.props;
     return (
       <div style={{ padding: '10px 45px' }}>
         <Head>
@@ -12,6 +34,10 @@ class Index extends React.Component {
           <meta name="description" content="This is a description" />
         </Head>
         <p>Content</p>
+        <p>
+          Email:
+          {` ${user.email}`}
+        </p>
       </div>
     );
   }
